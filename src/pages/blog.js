e0ebb725b86f6c263786cd9a19f74ccfc1700c02
js-tut <johnsmilga@gmail.com>
2019-06-16 16:17:53 -0700

    9-global-css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,10 @@ import Layout from '../components/layout'
 
 export default class blog extends Component {
   render() {
-    return <Layout>this is our blog page</Layout>
+    return (
+      <Layout>
+        <h1>this is our blog page</h1>
+      </Layout>
+    )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
+// global css
+// multiple elements
 import React from 'react'
 import Layout from '../components/layout'
 export default () => (
   <Layout>
-    <h1 style={{ color: 'red', textTransform: 'capitalize' }}>
-      this is our home page
-    </h1>
+    <h1>this is our home page</h1>
+    <h1>another heading</h1>
     Hello world!
   </Layout>
 )
