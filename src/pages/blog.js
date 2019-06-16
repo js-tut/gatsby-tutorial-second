ff17ac359571aa2302c14b1540a2d9958bc98a2e
js-tut <johnsmilga@gmail.com>
2019-06-15 21:11:38 -0700

    4-link-component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
-
+import { Link } from 'gatsby'
 export default class blog extends Component {
   render() {
-    return <div>this is our blog page</div>
+    return (
+      <div>
+        this is our blog page
+        <div>
+          <Link to="/">Home Page</Link>
+        </div>
+      </div>
+    )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { Link } from 'gatsby'
 export default () => (
   <div>
     <h1>this is our home page</h1>
     Hello world!
+    <div>
+      {/* <a href="/blog/">blog page</a> */}
+      <Link to="/blog/">blog page</Link>
+    </div>
+    <div>
+      <a href="https://www.gatsbyjs.org">gatsby page</a>
+    </div>
   </div>
 )
