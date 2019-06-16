2ec7a9a63621be324ba9e035b7edc2d90e0f066e
js-tut <johnsmilga@gmail.com>
2019-06-16 15:47:39 -0700

    8-inline-css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Layout from '../components/layout'
 export default () => (
   <Layout>
-    <h1>this is our home page</h1>
+    <h1 style={{ color: 'red', textTransform: 'capitalize' }}>
+      this is our home page
+    </h1>
     Hello world!
   </Layout>
 )
