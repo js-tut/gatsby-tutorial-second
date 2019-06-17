1aec698b5b1d441ffc3d97224d044c4694067598
js-tut <johnsmilga@gmail.com>
2019-06-16 22:52:16 -0700

    18-siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'tutorial',
+    description: 'just some description about our site',
+    author: '@johndoe',
+    data: ['item1', 'item2'],
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
