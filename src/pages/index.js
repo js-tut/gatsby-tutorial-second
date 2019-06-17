a29f8c2d440ad012219e37bad073c18c1c6688b5
js-tut <johnsmilga@gmail.com>
2019-06-16 18:09:14 -0700

    13-sass-install

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,5 @@ export default () => (
     <h1 className="title">another heading</h1>
     Hello world!
     <h2>this second heading</h2>
-    <div className="container">
-      <div className="row">
-        <div className="col-4">hello</div>
-        <div className="col-4">hello</div>
-        <div className="col-4">hello</div>
-      </div>
-    </div>
   </Layout>
 )
