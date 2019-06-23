ff6b0afff724f1a964ce3d848a4b460e92f3be15
js-tut <johnsmilga@gmail.com>
2019-06-22 19:31:40 -0700

    23-query-keyword

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-
-const Header = () => {
-  const {
-    site: { siteMetadata: info },
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-          data {
-            age
-          }
+const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteTitle: title
+        description
+        author
+        data {
+          age
         }
       }
     }
-  `)
+  }
+`
+const Header = () => {
+  const {
+    site: { siteMetadata: info },
+  } = useStaticQuery(getData)
   console.log(info)
-
+  const title = info.siteTitle
   return (
     <div>
       {/* <h1>author : {data.site.siteMetadata.author}</h1> */}
       <h1>author : {info.author}</h1>
-      <h1>title : {info.title}</h1>
+      <h1>title : {title}</h1>
     </div>
   )
 }
