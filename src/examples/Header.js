a947d7e0ede9865011d4de0192ab234abf8b865f
js-tut <johnsmilga@gmail.com>
2019-06-22 19:11:08 -0700

    21-useStaticQuery

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-const getData = graphql`
-  {
-    site {
-      siteMetadata {
-        siteTitle: title
-        description
-        author
-        data {
-          age
-        }
-      }
-    }
-  }
-`
+
 const Header = () => {
   const {
     site: { siteMetadata: info },
-  } = useStaticQuery(getData)
+  } = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          data {
+            age
+          }
+        }
+      }
+    }
+  `)
   console.log(info)
-  const title = info.siteTitle
+
   return (
     <div>
       {/* <h1>author : {data.site.siteMetadata.author}</h1> */}
       <h1>author : {info.author}</h1>
-      <h1>title : {title}</h1>
+      <h1>title : {info.title}</h1>
     </div>
   )
 }
