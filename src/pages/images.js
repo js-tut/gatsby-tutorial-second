eaa11f36c788160e231ae4bb70910216e74b81af
js-tut <johnsmilga@gmail.com>
2019-06-24 18:03:36 -0700

    35-render-images-prop

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Images from '../examples/Images'
 
 const images = () => {
-  return <Layout>this is images page</Layout>
+  return (
+    <Layout>
+      <Images />
+    </Layout>
+  )
 }
 
 export default images
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-
+import Images from '../components/Images'
 export default function products() {
   return (
     <Layout>
       <h1>this is products page</h1>
+      <Images />
     </Layout>
   )
 }
