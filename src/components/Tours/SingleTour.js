3f4ba70002f8d4919bbb123da07d493ea50cb1b1
js-tut <johnsmilga@gmail.com>
2019-06-29 22:33:59 -0700

    54-react-icons

--- src/components/Tours/SingleTour.js
+++ src/components/Tours/SingleTour.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { FaHome } from 'react-icons/fa'
 const SingleTour = () => {
-  return <div>this is a single tour</div>
+  return (
+    <div>
+      this is a single tour
+      <FaHome />
+    </div>
+  )
 }
 
 export default SingleTour
