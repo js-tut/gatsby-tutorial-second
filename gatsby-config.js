8b67e48d7c2098a5febbc876b01c22bc568a0ede
js-tut <johnsmilga@gmail.com>
2019-06-29 18:02:16 -0700

    env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -24,9 +27,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: 'aa6lmvvj10ja',
+        spaceId: process.env.CONTENTFUL_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: 'nVPmSFi1BcBubGwUWHkKinvn5cPFsanxf82UG7B_p54',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
