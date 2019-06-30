cb87c2503ea01a3857211a3676d83951e9067301
js-tut <johnsmilga@gmail.com>
2019-06-29 23:31:08 -0700

    56-prop-types

--- src/components/Tours/SingleTour.js
+++ src/components/Tours/SingleTour.js
@@ -3,6 +3,7 @@ import { FaMap } from 'react-icons/fa'
 import Image from 'gatsby-image'
 import styles from '../../css/tour.module.css'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
 
 const SingleTour = ({ tour }) => {
   const { name, price, country, days, slug, images } = tour
@@ -31,6 +32,15 @@ const SingleTour = ({ tour }) => {
       </div>
     </article>
   )
+}
+SingleTour.propTypes = {
+  tour: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }),
 }
 
 export default SingleTour
